664430e7a0bf1983f30d28fbcba072ed1d534308
OsaidM <Osaid.m1@gmail.com>
2021-02-20 18:53:20 +0200

    GamingHub-v0.1 adding details page

---
+++ views/ProductHero.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '../components/Button';
+import Typography from '../components/Typography';
+import ProductHeroLayout from './ProductHeroLayout';
+import AppAppBar from './AppAppBar';
+import ProductCategories from './ProductCategories';
+import ProductSmokingHero from './ProductSmokingHero';
+import ProductValues from './ProductValues';
+import ProductHowItWorks from './ProductHowItWorks';
+import ProductCTA from './ProductCTA';
+import AppFooter from './AppFooter';
+
+const backgroundImage =
+  'https://wearesocial-net.s3.amazonaws.com/wp-content/uploads/2020/11/gamer_room.jpg';
+
+const styles = (theme) => ({
+  background: {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundColor: '#7fc7d9', // Average color of the background image.
+    backgroundPosition: 'center',
+  },
+  button: {
+    minWidth: 200,
+  },
+  h5: {
+    marginBottom: theme.spacing(4),
+    marginTop: theme.spacing(4),
+    [theme.breakpoints.up('sm')]: {
+      marginTop: theme.spacing(10),
+    },
+  },
+  more: {
+    marginTop: theme.spacing(2),
+  },
+});
+
+function ProductHero(props) {
+  const { classes } = props;
+
+  return (
+    <>
+    <AppAppBar/>
+    <ProductHeroLayout backgroundClassName={classes.background}>
+      {/* Increase the network loading priority of the background image. */}
+      <img style={{ display: 'none' }} src={backgroundImage} alt="increase priority" />
+      <Typography color="inherit" align="center" variant="h2" marked="center">
+        Upgrade your Games
+      </Typography>
+      <Typography color="inherit" align="center" variant="h5" className={classes.h5}>
+        Enjoy our secret and special game mods !
+      </Typography>
+      <Button
+        color="secondary"
+        variant="contained"
+        size="large"
+        className={classes.button}
+        component="a"
+        href="/signup"
+      >
+        Register
+      </Button>
+      <Typography variant="body2" color="inherit" className={classes.more}>
+        Discover the experience
+      </Typography>
+    </ProductHeroLayout>
+    <ProductValues  />
+    <ProductCategories/>
+    <ProductHowItWorks/>
+    <ProductCTA  />
+    <ProductSmokingHero  />
+    <AppFooter/>
+    </>
+  );
+}
+
+ProductHero.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ProductHero);
