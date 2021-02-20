f0ed64bf718e23198caaf35d10020bcf10290b95
Nidal <nidaldannoon@gmail.com>
2021-02-20 20:12:39 +0200

    update

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,12 @@ import React from 'react';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 import ProductHero from './components/modules/views/ProductHero';
+import Archive from './components/Archive';
 import Privacy from './components/Privacy';
 import Terms from './components/Terms';
 import {Router} from '@reach/router'
 import PostForm from './components/PostForm';
+import Contact from './components/Contact';
 import About from './components/About';
 
 function App() {
@@ -17,9 +19,11 @@ function App() {
       <Router>
         <SignUp path = "/signup"/>
         <SignIn path = "/signin"/>
+		<Archive path = "/archive"/>
         <ProductHero path = "/"/>
         <Privacy path = "/privacy"/>
         <PostForm path= "/new"/>
+		<Contact path = "/contact"/>
         <About path= "/about"/>
         <Terms path = "/terms"/>
       </Router>
